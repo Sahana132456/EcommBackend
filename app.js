65d913acb6affefa97c9cb30ec3d5bfe5b9263f4
Sahana132456 <sahanapatil132@gmail.com>
2025-03-19 11:35:24 +0530

    final change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const {User} = require ('./model/User');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require  ('morgan');
-const bcrypt = require ('bcrypt');
+const bcrypt = require ('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {Product} = require('./model/Product');
 const {Cart} = require('./model/Cart');
@@ -14,8 +14,10 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'))
 
+// Vp61wmTPEfJUrtaH
 
-mongoose.connect('mongodb://127.0.0.1:27017/kleProject')
+let MONGODB_URL="mongodb+srv://sahanapatil132:Vp61wmTPEfJUrtaH@cluster0.dp05p.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(MONGODB_URL)
 .then(()=>{
     console.log("DB is connected")
 }).catch((err)=>{
